bef161cd923272aabc8bff9452fd5e88df78bc8c
zinhart <aromzinhartdegraca@gmail.com>
2022-12-13 20:44:16 -0800

    Save point dumped tables neately

--- concord/part-1/Client.java
+++ concord/part-1/Client.java
@@ -22,6 +22,68 @@ public class Client {
          System.exit(0);
       }
    }
+   public static void getColumns(Connection c, String table) {
+      System.out.println(String.format("(+) Attempting to retrieve column names from table: %s", table));
+      try {
+         Statement stmt = null;
+         String sql = String.format("SELECT column_name, data_type FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '%s'", table);
+         stmt = c.createStatement();
+         ResultSet rs = stmt.executeQuery(sql);
+         while ( rs.next() ) {
+            System.out.println(String.format("(+) Column Name: %s | Datatype: %s",rs.getString("column_name"), rs.getString("data_type")));
+         }
+         rs.close();
+         stmt.close();
+      } catch(Exception e) {
+         System.err.println("(-) Failure in getColumns");
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+   }
+   public static String ezFormat(Object... args) {
+      String format = new String(new char[args.length]).replace("\0", "%s;");
+      return String.format(format, args);
+   }
+   public static void dumpTable(Connection c, String table, String ... columns) {
+      //System.out.println(ezFormat(columns));
+      System.out.println(String.format("(+) Attempting to dump table: %s", table));
+      String colUnpacked = "";
+      System.out.print("(+) ");
+      for (int i = 0; i < columns.length; ++i) {
+         if (i < columns.length - 1) {
+            colUnpacked += columns[i];
+            colUnpacked += ",";
+            System.out.print(columns[i]);
+            System.out.print(" ");
+         }
+         else {
+            colUnpacked += columns[i];
+            System.out.print(columns[i]);
+         }
+
+      }
+      System.out.println("");
+      try {
+         Statement stmt = null;
+         String sql = String.format("SELECT %s FROM %s", colUnpacked, table);
+         stmt = c.createStatement();
+         ResultSet rs = stmt.executeQuery(sql);
+         while ( rs.next() ) {
+            System.out.print("(+) ");
+            for (int i = 0; i < columns.length; ++i) {
+               System.out.print(String.format("%s ", rs.getString(columns[i])));
+            }
+            System.out.println("");
+         }
+         rs.close();
+         stmt.close();
+      } catch(Exception e) {
+         System.err.println("(-) Failure in getColumns");
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+      
+   }
    public static void main(String args[]) {
       Connection c = null;
       String db = "";
@@ -37,7 +99,20 @@ public class Client {
          Class.forName("org.postgresql.Driver");
          String url = String.format("jdbc:postgresql://%s:%s/postgres", db, port);
          c = DriverManager.getConnection(url, "postgres", "quake1quake2quake3arena");
-         getTables(c);
+         //getTables(c);
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getColumns(c, "users");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getColumns(c, "api_keys");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         getColumns(c, "secrets");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         dumpTable(c, "users", "user_id", "username", "display_name", "user_type", "user_email", "is_disabled", "last_group_sync_dt", "domain");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         dumpTable(c, "api_keys", "user_id", "api_key", "key_id", "key_name", "expired_at", "last_notified_at");
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------");
+         dumpTable(c, "secrets", "secret_name", "secret_type", "secret_data", "encrypted_by", "secret_id", "org_id", "owner_id", "visibility", "store_type", "project_id");
+
          c.close();
       } catch (Exception e) {
          e.printStackTrace();
