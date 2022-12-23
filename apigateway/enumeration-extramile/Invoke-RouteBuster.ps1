fbef62077db71f0d744a7b2316b37fcd15db94b4
zinhart <aromzinhartdegraca@gmail.com>
2022-12-22 16:34:16 -0800

    Adding extramile powershell scripts for enumeration

--- apigateway/enumeration-extramile/Invoke-RouteBuster.ps1
+++ apigateway/enumeration-extramile/Invoke-RouteBuster.ps1
@@ -23,28 +23,30 @@ function Invoke-RouteBuster() {
   $actions_list = Get-Content $ActionList
   $word_list = Get-Content $Wordlist
   $ignore_status_codes = 204,401,403,404
-  $results = New-Object -TypeName "System.Collections.ArrayList"
-  for($i = 0; $i -lt $word_list.Length; ++$i ) {
-    $outer_percent_complete = [System.Math]::Round($i / $word_list.Length * 100)
-    Write-Progress -Id 1 -Activity "Current word: $($word_list[$i])" -Status "$outer_percent_complete% Complete:" -PercentComplete $outer_percent_complete;
-    for($j = 0; $j -lt $actions_list.Length; ++$j ) {
-      $inner_percent_complete = [System.Math]::Round($j / $actions_list.Length * 100,2)
-      Write-Progress -ParentId 1 -Activity "Current url: $($actions_list[$j])" -Status "$inner_percent_complete% Complete:" -PercentComplete $inner_percent_complete;
-      $url = "http://apigateway:8000/files/import" #"$target/$word_list[$i]/$action_list[$j]"
+  foreach($word in $word_list) {
+    foreach($action in $actions_list) {
+      $url = "http://apigateway:8000/files/import" #"$target/$word/$action"
       #write-output $url
       $res_get = Invoke-WebRequest -Uri $url -Method Get -SkipHttpErrorCheck
       $res_post = Invoke-WebRequest -Uri $url -Method Post -SkipHttpErrorCheck
-      if( ($res_get.StatusCode -notin $ignore_status_codes) -or ($res_post.StatusCode -notin $ignore_status_codes)) {
+      $res_put = Invoke-WebRequest -Uri $url -Method Put -SkipHttpErrorCheck
+      $res_patch = Invoke-WebRequest -Uri $url -Method Patch -SkipHttpErrorCheck
+      <#if( ($res_get.StatusCode -notin $ignore_status_codes) -or ($res_post.StatusCode -notin $ignore_status_codes) -or ($res_put.StatusCode -notin $ignore_status_codes -or ($res_patch.StatusCode -notin $ignore_status_codes))#>
+if( ($res_get.StatusCode -notin $ignore_status_codes) -or ($res_post.StatusCode -notin $ignore_status_codes)
+      ) {
         $props = [ordered]@{
           URI = $url
           GET = $res_get.StatusCode
           POST = $res_post.StatusCode
+          PUT = $res_put.StatusCode
+          PATCH = $res_patch.StatusCode
         }
         $result = New-Object -TypeName PSObject -Property $props
-        [void]$results.Add($result)
+        write-output $result | FT
+        break
       }
     }
+    break
   }
-  Write-Output $results | Format-Table -AutoSize
   
 }
