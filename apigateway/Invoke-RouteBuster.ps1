eb27c8bc031994c063fa1a87f61b39c99cc80b34
zinhart <aromzinhartdegraca@gmail.com>
2022-12-25 08:02:11 -0800

    Update

--- apigateway/Invoke-RouteBuster.ps1
+++ apigateway/Invoke-RouteBuster.ps1
@@ -23,28 +23,29 @@ function Invoke-RouteBuster() {
   $actions_list = Get-Content $ActionList
   $word_list = Get-Content $Wordlist
   $ignore_status_codes = 204,401,403,404
-  $results = New-Object -TypeName "System.Collections.ArrayList"
   for($i = 0; $i -lt $word_list.Length; ++$i ) {
     $outer_percent_complete = [System.Math]::Round($i / $word_list.Length * 100)
-    Write-Progress -Id 1 -Activity "Current word: $($word_list[$i])" -Status "$outer_percent_complete% Complete:" -PercentComplete $outer_percent_complete;
+    Write-Progress -Id 1 -Activity "Current Word: $($word_list[$i])" -Status "$outer_percent_complete% Complete:" -PercentComplete $outer_percent_complete;
     for($j = 0; $j -lt $actions_list.Length; ++$j ) {
+      $url = "$($target)/$($word_list[$i])/$($actions_list[$j])"
+      #$url = "$($target)/files/import"
       $inner_percent_complete = [System.Math]::Round($j / $actions_list.Length * 100,2)
-      Write-Progress -ParentId 1 -Activity "Current url: $($actions_list[$j])" -Status "$inner_percent_complete% Complete:" -PercentComplete $inner_percent_complete;
-      $url = "http://apigateway:8000/files/import" #"$target/$word_list[$i]/$action_list[$j]"
+      Write-Progress -ParentId 1 -Activity "Url: $url" -Status "$inner_percent_complete% Complete:" -PercentComplete $inner_percent_complete;
       #write-output $url
       $res_get = Invoke-WebRequest -Uri $url -Method Get -SkipHttpErrorCheck
       $res_post = Invoke-WebRequest -Uri $url -Method Post -SkipHttpErrorCheck
+      $res_get | Select-Object -Property *
       if( ($res_get.StatusCode -notin $ignore_status_codes) -or ($res_post.StatusCode -notin $ignore_status_codes)) {
         $props = [ordered]@{
           URI = $url
           GET = $res_get.StatusCode
           POST = $res_post.StatusCode
+          GET_RES = $res_get
+          POST_RES = $res_post
         }
-        $result = New-Object -TypeName PSObject -Property $props
-        [void]$results.Add($result)
+        $found = New-Object -TypeName PSObject -Property $props
+        Write-Output $found | Format-List
       }
     }
   }
-  Write-Output $results | Format-Table -AutoSize
-  
 }
